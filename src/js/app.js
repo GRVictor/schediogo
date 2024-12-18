d71a42be9b90cabd81d3863d14f4e7dcf1e6a1f2
Victor García <grojasvictor@gmail.com>
2024-12-18 15:06:01 -0600

    Botones con funcionalidad

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
 let step = 1;
+const firstStep = 1;
+const lastStep = 3;
 
 document.addEventListener('DOMContentLoaded', function() {
     startApp();
@@ -7,6 +9,9 @@ document.addEventListener('DOMContentLoaded', function() {
 function startApp() {
     showSection(); // Show section with a specific step
     tabs(); // Change between tabs
+    buttonsPager(); // Add or remove buttons to the pager
+    nextPage(); // Go to the next page
+    previousPage(); // Go to the previous page
 }
 
 function showSection() {
@@ -15,6 +20,7 @@ function showSection() {
     if (previousSection) {
         previousSection.classList.remove('show');
     }
+
     // Select section with a specific step
     const stepSelector = `#step-${step}`;
     const section = document.querySelector(stepSelector);	
@@ -38,6 +44,44 @@ function tabs() {
         button.addEventListener('click', function(e) {
             step = parseInt(e.target.dataset.step);
             showSection();
+            buttonsPager();
         });
+    });
+}
+
+function buttonsPager() {
+    const nextPage = document.querySelector('#next');
+    const previousPage = document.querySelector('#prev');
+
+    if(step === firstStep) {
+        previousPage.classList.add('hidden');
+        nextPage.classList.remove('hidden');
+    } else if (step === lastStep) {
+        previousPage.classList.remove('hidden');
+        nextPage.classList.add('hidden');
+    } else {
+        previousPage.classList.remove('hidden');
+        nextPage.classList.remove('hidden');
+    }
+
+}
+
+function previousPage() {
+    const previousPage = document.querySelector('#prev');
+    previousPage.addEventListener('click', function() {
+        if (step <= firstStep) return;
+        step--;
+        buttonsPager();
+        showSection();
+    });
+}
+
+function nextPage() {
+    const nextPage = document.querySelector('#next');
+    nextPage.addEventListener('click', function() {
+        if (step >= lastStep) return;
+        step++;
+        buttonsPager();
+        showSection();
     });
 }
